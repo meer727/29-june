4f8237edb187b76b7a7d72679068fdd3e74a63ee
Mansoor Alam <90790553+meer727@users.noreply.github.com>
2025-06-29 09:55:42 +0530

    Update outputs.tf

--- mongodb-terraform/modules/route-table/outputs.tf
+++ mongodb-terraform/modules/route-table/outputs.tf
@@ -1,4 +1,9 @@
-output "route_table_id" {
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
   description = "ID of the private route table"
   value       = aws_route_table.private_rt.id
 }
