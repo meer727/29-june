aa7d89b24105f0d8462e89d5297daf9a15a871ca
Mansoor Alam <90790553+meer727@users.noreply.github.com>
2025-06-29 09:48:08 +0530

    Update main.tf

--- mongodb-terraform/modules/route-table/main.tf
+++ mongodb-terraform/modules/route-table/main.tf
@@ -1,1 +1,16 @@
-# route table
+resource "aws_route_table" "private_rt" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = var.nat_gateway_id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private_rt.id
+}
