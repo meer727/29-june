a966415f38df0f563b63bfe60799396116c86a10
Mansoor Alam <90790553+meer727@users.noreply.github.com>
2025-06-29 09:59:13 +0530

    Update main.tf

--- mongodb-terraform/modules/nat-gateway/main.tf
+++ mongodb-terraform/modules/nat-gateway/main.tf
@@ -10,19 +10,4 @@ resource "aws_nat_gateway" "nat_gw" {
   }
 }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = var.vpc_id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-  tags = {
-    Name = "private-rt"
-  }
-}
 
-resource "aws_route_table_association" "private_rt_assoc" {
-  count          = length(var.private_subnet_ids)
-  subnet_id      = var.private_subnet_ids[count.index]
-  route_table_id = aws_route_table.private_rt.id
-}
