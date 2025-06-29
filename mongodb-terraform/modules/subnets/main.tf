724a899795dd990a905b9acb436e9d01ee143911
Mansoor Alam <90790553+meer727@users.noreply.github.com>
2025-06-29 09:58:07 +0530

    Update main.tf

--- mongodb-terraform/modules/subnets/main.tf
+++ mongodb-terraform/modules/subnets/main.tf
@@ -21,9 +21,3 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
-# Associate Public Subnets with Public Route Table
-resource "aws_route_table_association" "public_rt_assoc" {
-  count          = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = var.public_route_table_id
-}
